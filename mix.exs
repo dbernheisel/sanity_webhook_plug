1fd74b0c7d836fe9db4bf7f45d3b6cb2708ec337
David Bernheisel <david@bernheisel.com>
2023-01-05 14:31:27 -0500

    Add credo to :test

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SanityWebhookPlug.MixProject do
       app: :sanity_webhook_plug,
       version: "0.1.0",
       elixir: "~> 1.14",
-      preferred_cli_env: [dialyzer: :test, tests: :test],
+      preferred_cli_env: [dialyzer: :test, credo: :test, tests: :test],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
